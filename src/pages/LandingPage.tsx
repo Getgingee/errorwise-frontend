7e7852eca3b2b4405dc28cc51fbd0e0d06ca75a1
pankaj kumar <Pankajkrjain@outlook.com>
2025-11-02 16:02:41 +0530

    refactor: update Twitter link to X (x.com)

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -546,11 +546,11 @@ const LandingPage: React.FC = () => {
                   <Github size={22} />
                 </a>
                 <a 
-                  href="https://twitter.com/errorwise" 
+                  href="https://x.com/errorwise" 
                   target="_blank" 
                   rel="noopener noreferrer"
                   className="text-gray-400 hover:text-cyan-400 hover:drop-shadow-[0_0_8px_rgba(34,211,238,0.6)] transition-all duration-300"
-                  aria-label="Twitter"
+                  aria-label="X (formerly Twitter)"
                 >
                   <Twitter size={22} />
                 </a>
