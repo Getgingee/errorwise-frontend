be7e33eb13b649fb33071c518a8bd1118bb8488d
Getgingee <pankaj@getgingee.com>
2025-11-10 19:43:22 +0530

    fix: Update UI/index.ts to use lowercase button and input imports

--- src/components/UI/index.ts
+++ src/components/UI/index.ts
@@ -1,6 +1,6 @@
 // UI Component exports
-export { default as Button } from './Button';
-export { default as Input } from './Input';
+export { default as Button } from './button';
+export { default as Input } from './input';
 export { default as Modal } from './Modal';
 export { default as Toast, ToastContainer } from './Toast';
 export { default as LoadingSpinner } from './LoadingSpinner';
