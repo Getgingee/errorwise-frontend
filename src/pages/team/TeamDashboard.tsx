ab6e3a2041bb4121ebe62219f795dfe96c424aec
Getgingee <pankaj@getgingee.com>
2025-12-06 01:23:34 +0530

    chore: Update team feature descriptions

--- src/pages/team/TeamDashboard.tsx
+++ src/pages/team/TeamDashboard.tsx
@@ -273,13 +273,13 @@ export const TeamDashboard = () => {
               <svg className="w-5 h-5 mr-2 text-blue-600 flex-shrink-0 mt-0.5" fill="currentColor" viewBox="0 0 20 20">
                 <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
               </svg>
-              <span>30-minute video chat sessions with unlimited participants</span>
+              <span>Video meetings with screen sharing and collaborative notes</span>
             </li>
             <li className="flex items-start">
               <svg className="w-5 h-5 mr-2 text-blue-600 flex-shrink-0 mt-0.5" fill="currentColor" viewBox="0 0 20 20">
                 <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
               </svg>
-              <span>Screen sharing and collaborative debugging</span>
+              <span>Schedule meetings or start instant video calls</span>
             </li>
             <li className="flex items-start">
               <svg className="w-5 h-5 mr-2 text-blue-600 flex-shrink-0 mt-0.5" fill="currentColor" viewBox="0 0 20 20">
@@ -301,6 +301,7 @@ export const TeamDashboard = () => {
 };
 
 export default TeamDashboard;
+
 
 
 
